26eea9e2675fd93f1f45fc183c321577a5ce1104
AdrianLupu21 <45321630+AdrianLupu21@users.noreply.github.com>
2018-11-26 17:44:51 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import LandingPage from './components/landingpage';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <LandingPage />
+      </div>
+    );
+
+  }
+}
+
+export default App;
